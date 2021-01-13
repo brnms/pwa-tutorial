d4c6b54109ec6d582827515ab69f19980d5f6db2
Bruno Santos <bruno.matasantos@gmail.com>
2021-01-13 17:44:38 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,3 +2,13 @@
 self.addEventListener('install', evt => {
     console.log('service worker has been installed')
 })
+
+// activate event 
+self.addEventListener('activate', avt => {
+    console.log('service worker has been activated')
+}); 
+
+// fetch event
+self.addEventListener('fetch', evt => {
+    console.log('fetch event', evt)
+})
