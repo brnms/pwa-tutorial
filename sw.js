e78d2902784f01c1f7b3a267b7b0a07bad82061c
Bruno Santos <bruno.matasantos@gmail.com>
2021-01-21 15:07:59 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const staticCacheName = 'site-static'
+const staticCacheName = 'site-static-v2'
+const dynamicCacheName = 'site-dynamic-v1'
 const assets = [
     '/',
     '/index.html',
@@ -9,7 +10,8 @@ const assets = [
     '/css/materialize.min.css',
     '/img/dish.png',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'https://fonts.gstatic.com/s/materialicons/v70/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+    'https://fonts.gstatic.com/s/materialicons/v70/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+    'pages/fallback.html'
 ];
 
 // initial service worker
@@ -25,8 +27,17 @@ self.addEventListener('install', evt => {
 });
 
 // activate event 
-self.addEventListener('activate', avt => {
-    console.log('service worker has been activated')
+self.addEventListener('activate', evt => {
+    //console.log('service worker has been activated')
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys)
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)    
+                .map(key => caches.delete(key))
+            )
+        })
+    )
 }); 
 
 // fetch event
@@ -34,7 +45,17 @@ self.addEventListener('fetch', evt => {
    //console.log('fetch event', evt)
    evt.respondWith(
        caches.match(evt.request).then(cacheRes => {
-           return cacheRes || fetch(evt.request)
+           return cacheRes || fetch(evt.request).then(fetchRes => {
+               return caches.open(dynamicCacheName).then(cache => {
+                   cache.put(evt.request.url, fetchRes.clone())
+                   return fetchRes;
+               })
+           })
+       }).catch(() => {
+           if(evt.request.url.indexOf('.html') > -1){
+                caches.match('/pages/fallback.html')  
+           }
+            
        })
    );
 })
